8a8ff1b443c718dcd73cd0244534ba1e6e3911da
huangbilian <huangbilian@jasic.com.cn>
2026-05-07 11:12:29 +0800

    1、新增订阅消息 2、修改ip

--- mp/aftersale/src/constants/subscribeMessage.ts
+++ mp/aftersale/src/constants/subscribeMessage.ts
@@ -5,5 +5,17 @@
 export const WECHAT_TMPL_CUSTOMER_EVALUATION_INVITE =
   '01ZBgiyxkgui_wKWFtYsETnkSySMxeANaK2SoShvXkM'
 
+/** 微信订阅消息模板 ID（新增） */
+export const WECHAT_TMPL_EXTRA_NOTICE_1 =
+  '_p97aAe9-FJ2c6lCcZjVMQgxDnvBz8q6IRdFnnjIyWg'
+
+/** 微信订阅消息模板 ID（新增） */
+export const WECHAT_TMPL_EXTRA_NOTICE_2 =
+  '0_vY_Wlie3dIuqmfpPAp_Hpbj-9yCso8yO1WSzWg3og'
+
 /** `uni.requestSubscribeMessage` 的 tmplIds，单次最多 3 个 */
-export const EVALUATION_INVITE_SUBSCRIBE_TMPL_IDS = [WECHAT_TMPL_CUSTOMER_EVALUATION_INVITE] as const
+export const EVALUATION_INVITE_SUBSCRIBE_TMPL_IDS = [
+  WECHAT_TMPL_CUSTOMER_EVALUATION_INVITE,
+  WECHAT_TMPL_EXTRA_NOTICE_1,
+  WECHAT_TMPL_EXTRA_NOTICE_2,
+] as const
