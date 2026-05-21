0c175e8d0d2b6db6fd81c8ad283e2a572d77461e
huangbilian <huangbilian@jasic.com.cn>
2026-05-21 16:28:34 +0800

    1、首页点击刷新时，需要重新请求数据

---
+++ jasic-ui-antd/src/views/home/composables/use-home-dashboard-on-mount.ts
@@ -0,0 +1,11 @@
+import { onMounted, type Ref } from 'vue';
+
+/**
+ * 首页看板挂载拉取：首次进入请求一次；页签栏「刷新」会 remount 当前页，
+ * 但看板 state 为模块级单例且 loaded 仍为 true，须传 force 才会重新打首页聚合接口。
+ */
+export function useHomeDashboardOnMount(load: (force?: boolean) => Promise<void> | void, loaded: Ref<boolean>) {
+  onMounted(() => {
+    void load(loaded.value);
+  });
+}
